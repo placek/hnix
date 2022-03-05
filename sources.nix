7bd32fae2f6dfe34d529fc647a70ccd3b13b02a9
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-03-05 12:40:45 +0100

    fixes for nix configuration

--- sources.nix
+++ sources.nix
@@ -1,7 +1,8 @@
 {
   nixpkgs = builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs.git";
-    rev = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
-    ref = "refs/tags/21.11";
+    url     = "https://github.com/NixOS/nixpkgs.git";
+    rev     = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
+    ref     = "refs/tags/21.11";
+    allRefs = true;
   };
 }
