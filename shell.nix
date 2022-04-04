58e9c9da19b1898d366445ce2e9616f9d713c1f5
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-04-04 22:22:21 +0200

    add hlint to nix config

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { name }:
 let
   project  = import ./release.nix { name = name; };
-  devTools = with project.nixpkgs; [ cabal-install haskell-language-server ];
+  devTools = with project.nixpkgs; [ cabal-install haskell-language-server hlint ];
 in
   project.env.overrideAttrs (oldEnv: { nativeBuildInputs = oldEnv.nativeBuildInputs ++ devTools; })
